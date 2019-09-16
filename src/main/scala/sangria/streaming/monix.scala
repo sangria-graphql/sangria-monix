70f4de10fb8b1aa6be3ed023a54a481eb8ae1bf6
Travis Brown <travisrobertbrown@gmail.com>
2019-09-16 02:37:28 -0500

    Replace deprecated Unicode arrows

--- src/main/scala/sangria/streaming/monix.scala
+++ src/main/scala/sangria/streaming/monix.scala
@@ -12,28 +12,28 @@ object monix {
   class ObservableSubscriptionStream(implicit scheduler: Scheduler) extends SubscriptionStream[Observable] {
     def supported[T[_]](other: SubscriptionStream[T]) = other.isInstanceOf[ObservableSubscriptionStream]
 
-    def map[A, B](source: Observable[A])(fn: A ⇒ B) = source.map(fn)
+    def map[A, B](source: Observable[A])(fn: A => B) = source.map(fn)
 
     def singleFuture[T](value: Future[T]) =
       Observable.fromFuture(value)
 
     def single[T](value: T) = Observable(value)
 
-    def mapFuture[A, B](source: Observable[A])(fn: A ⇒ Future[B]) =
-      source.mergeMap(a ⇒ Observable.fromFuture(fn(a)))
+    def mapFuture[A, B](source: Observable[A])(fn: A => Future[B]) =
+      source.mergeMap(a => Observable.fromFuture(fn(a)))
 
     def first[T](s: Observable[T]) =
       s.firstOrElseL(throw new IllegalStateException("Promise was not completed - observable haven't produced any elements.")).runToFuture
 
     def failed[T](e: Throwable) = Observable.raiseError(e)
 
-    def onComplete[Ctx, Res](result: Observable[Res])(op: ⇒ Unit) =
+    def onComplete[Ctx, Res](result: Observable[Res])(op: => Unit) =
       result.guaranteeCase {
         case ExitCase.Error(e) => Task(op)
         case _ => Task(op)
       }
 
-    def flatMapFuture[Ctx, Res, T](future: Future[T])(resultFn: T ⇒ Observable[Res]) =
+    def flatMapFuture[Ctx, Res, T](future: Future[T])(resultFn: T => Observable[Res]) =
       Observable.fromFuture(future).mergeMap(resultFn)
 
     def merge[T](streams: Vector[Observable[T]]) =
@@ -42,8 +42,8 @@ object monix {
       else
         throw new IllegalStateException("No streams produced!")
 
-    def recover[T](stream: Observable[T])(fn: Throwable ⇒ T) =
-      stream onErrorRecover {case e ⇒ fn(e)}
+    def recover[T](stream: Observable[T])(fn: Throwable => T) =
+      stream onErrorRecover {case e => fn(e)}
   }
 
   implicit def observableSubscriptionStream(implicit scheduler: Scheduler): SubscriptionStream[Observable] =
